815b2878bcec86aaec496a5c83b0939a67364474
po3tand3rson <bernardokm@vcu.edu>
2025-11-05 09:16:38 -0500

    Add files via upload

---
+++ CP_No_Storage.ino
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2017 Limor Fried for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#include <Adafruit_CircuitPlayground.h>
+
+void setup() {
+  // Initialize the circuit playground
+  CircuitPlayground.begin();
+}
+
+void loop() {
+  // If the left button is pressed....
+  if (CircuitPlayground.leftButton()) {
+     int r = random(0, 255);
+      int g = random(0, 255);
+      int b = random(0, 255);
+  }
+
+  //platground board makes sound when moved
+  float x = CircuitPlayground.motionX();
+  float y = CircuitPlayground.motionY();
+  float z = CircuitPlayground.motionZ();
+
+  //movement speed 
+  float magnitude = sqrt(x * x + y * y + z * z);
+
+  //motion indicator
+  if (magnitude > 15) {
+    //pitch adjustor!
+    int frequency = map((int)magnitude, 15, 50, 400, 1200);
+    frequency = constrain(frequency, 400, 1200);
+    CircuitPlayground.playTone(frequency, 150); // Play short tone
+  }
+
+  //loop delay so i don't go crazy
+  delay(50);
+}
+
