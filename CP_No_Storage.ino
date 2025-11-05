9b6d5dae60ab3545d7c7f211366129548d41d15b
po3tand3rson <bernardokm@vcu.edu>
2025-11-05 09:34:14 -0500

    Update CP_No_Storage.ino

--- CP_No_Storage.ino
+++ CP_No_Storage.ino
@@ -7,10 +7,10 @@ void setup() {
 
 void loop() {
   // If the left button is pressed....
-  if (CircuitPlayground.leftButton()) {
-     int r = random(0, 255);
-      int g = random(0, 255);
-      int b = random(0, 255);
+  if(CircuitPlayground.leftButton()) {
+    int r = random(0, 255);
+    int g = random(0, 255);
+    int b = random(0, 255);
   }
 
   //playground board makes sound when moved
@@ -25,11 +25,10 @@ void loop() {
   if (magnitude > 15) {
     //pitch adjustor!
     int frequency = map((int)magnitude, 15, 50, 400, 1200);
-    frequency = constrain(frequency, 400, 1200);
+    //frequency = constrain(frequency, 400, 1200);
     CircuitPlayground.playTone(frequency, 150); // Play short tone
   }
 
   //loop delay so i don't go crazy
   delay(50);
 }
-
